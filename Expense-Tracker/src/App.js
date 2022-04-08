3078841d6ddd385f3265542ae9271ec1e0ac9ec8
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-04-09 05:02:37 +0900

    refactor: organize component files

--- Expense-Tracker/src/App.js
+++ Expense-Tracker/src/App.js
@@ -1,4 +1,6 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -22,6 +24,13 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, { expenses })
+  // );
 
   return (
     <div>
--- Expense-Tracker/src/components/Expenses/Expenses.js
+++ Expense-Tracker/src/components/Expenses/Expenses.js
@@ -1,3 +1,4 @@
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 
 import "./Expenses.css";
@@ -6,7 +7,7 @@ function Expenses(props) {
   const expenses = props.expenses;
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
@@ -27,7 +28,7 @@ function Expenses(props) {
         amount={expenses[3].amount}
         date={expenses[3].date}
       ></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 
