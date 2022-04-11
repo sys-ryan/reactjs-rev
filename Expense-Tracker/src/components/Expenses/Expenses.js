fb869ccf62fae2297b61937a276f3eef612ac1b1
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-04-11 15:55:43 +0900

    refactor(Expenses): create ExpensesList copmonent for list rendering logic in Expenses component
    - create ExpensesList component
    - modify ExpenseItem component to have root <li> tag

--- Expense-Tracker/src/components/Expenses/Expenses.js
+++ Expense-Tracker/src/components/Expenses/Expenses.js
@@ -1,7 +1,8 @@
-import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import React, { useState } from "react";
+
+import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 import "./Expenses.css";
 
@@ -18,23 +19,11 @@ const Expenses = (props) => {
     (expense) => expense.date.getFullYear().toString() === filteredYear.toString()
   );
 
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className="expenses">
         <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {expensesContent}
+        <ExpensesList filteredExpenses={filteredExpenses} />
       </Card>
     </div>
   );
