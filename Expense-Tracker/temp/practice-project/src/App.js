804a3bd4097fb2784af731bb38fb856f269fc00a
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-04-13 12:03:57 +0900

    practice prj

---
+++ Expense-Tracker/temp/practice-project/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const addUserHandler = (newUser) => {
+    setUsers((prevUsersList) => [newUser, ...prevUsersList]);
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={users} />
+    </div>
+  );
+}
+
+export default App;
