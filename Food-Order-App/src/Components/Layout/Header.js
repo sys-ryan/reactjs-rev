e8b8bf82c4238322f23681d1e6a60861ee4e54cf
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-04-20 22:09:36 +0900

    feat: meal
    - MealItem
    - AvailableMeals
    - Meals
    - MealsList
    - MealsSUmmary

--- Food-Order-App/src/Components/Layout/Header.js
+++ Food-Order-App/src/Components/Layout/Header.js
@@ -2,6 +2,8 @@ import { Fragment } from "react";
 
 import classes from "./Header.module.css";
 
+import HeaderCartButton from "./HeaderCartButton";
+
 import mealsImage from "../../assets/meals.jpeg";
 
 const Header = (props) => {
@@ -9,7 +11,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="table" />
