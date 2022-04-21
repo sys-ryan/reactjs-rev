64acae6a76759f4dd5efec8d6051e75a3f489ba7
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-04-21 17:52:22 +0900

    feat: create add/remove item, cart animation

--- Food-Order-App/src/Components/Layout/HeaderCartButton.js
+++ Food-Order-App/src/Components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
@@ -9,8 +9,28 @@ const HeaderCartButton = (props) => {
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    //cleanup function
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`;
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
